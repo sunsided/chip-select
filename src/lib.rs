38a80156b5a29ec6ebe0332bd6004db17fbf0024
Markus Mayer <widemeadows@gmail.com>
2024-07-05 18:06:14 +0200

    Add feature gate for HAL 1.0 and 0.2

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,14 @@
 #![forbid(unsafe_code)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 
+#[cfg(all(feature = "hal-0_2", feature = "hal-1_0"))]
+compile_error!(
+    "HAL feature \"hal-0_2\" and feature \"hal-1_0\" cannot be enabled at the same time"
+);
+
+#[cfg(not(any(feature = "hal-0_2", feature = "hal-1_0")))]
+compile_error!("A HAL feature (\"hal-0_2\" or \"hal-1_0\") must be enabled");
+
 /// A chip-select trait.
 pub trait ChipSelect {
     /// Indicates whether this instance is configured to auto-select the chip on communication.
